6d4094c740fe2ce69ab8b51a41c7a3dea12fe60d
notesofdabbler <shanki@Shankars-MacBook-Pro.local>
2018-10-21 11:07:33 -0400

    code to get ids for different input types in shiny ui.R

---
+++ getuifnidlist.R
@@ -0,0 +1,24 @@
+#
+# Ability to get ids for different type of inputs (like selectInput, numericInput etc.)
+# from ui.R file
+#
+
+getid = function(x) {
+      id = strsplit(x, ",")[[1]][1]
+      idclean = gsub("\"","",y2)
+      return(idclean)
+      }
+
+getuifnId = function(uistr, uifn) {
+  uistrsplit = strsplit(x, paste0(uifn, "\\("))[[1]][-1]
+  uifnid = rep(NA, length(uistrsplit))
+  for(i in 1:length(uistrsplit)) {
+      uifnid[i] = getid(uistrsplit[i])
+  }
+  return(uifnid)
+}
+
+# tested with ui.R from
+# http://shiny.rstudio.com/gallery/update-input-demo.html
+x = paste(readLines("ui.R"), collapse = " ")
+getuifnId(x, "sliderInput")
